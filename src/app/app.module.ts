6563d0ca2d3b9835545bd8b6e9cdc152a971705b
murugean p <murugesanp@live.com>
2022-02-16 10:46:35 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopnavComponent } from './mastertheme/topnav/topnav.component';
+import { AsidenavComponent } from './mastertheme/asidenav/asidenav.component';
+import { FooterComponent } from './mastertheme/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopnavComponent,
+    AsidenavComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
