1b4227173fcea6b8bc709cebe9bf7e7871833925
murugesangithub <murugesan.net@gmail.com>
2022-02-17 10:10:33 +0530

    uncommented in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,7 +35,7 @@ children:[{path:'adddoctor',component:AdddoctorComponent,pathMatch:'full'},
 {path:'appointment',
 children:[{path:'addappoinment',component:AddappoinmentComponent,pathMatch:'full'},
 {path:'allappointment',component:AllappoinmentComponent,pathMatch:'full'},
-//{path:'editappointment',component:EditappointmentComponent,pathMatch:'full'},
+{path:'editappointment',component:EditappointmentComponent,pathMatch:'full'},
  {path:'cancelappointment',component:CancelappointmentComponent,pathMatch:'full'},
 ]
 },
